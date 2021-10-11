97ed77b0e162d4a8a97bb317274ea73fd30de14e
Amit Kumar <85798979+Amit-Kumar1219@users.noreply.github.com>
2021-10-11 19:32:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,27 +3,28 @@ const hoursEl = document.getElementById('hours');
 const minsEl = document.getElementById('mins');
 const secondEl = document.getElementById('seconds');
 
+//Enter any Date from which you want to see the days remaining
 const newYears = "1 Jan 2021";
 
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600 ) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days; 
-    hoursEl.innerHTML = hours;
-    minsEl.innerHTML = minutes;
-    secondEl.innerHTML = seconds;
+    daysEl.innerHTML = formatTime(days); 
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondEl.innerHTML = formatTime(seconds);
 }
-// function formatTime(time){
-//     return time < 10 ? ('0${time}') : time;
-// }
+ function formatTime(time){
+     return time < 10 ? ("0"+time) : time;
+ }
 countdown();
 
 setInterval(countdown, 1000);
